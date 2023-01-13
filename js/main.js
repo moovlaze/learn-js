4b3c5d90c451937325968464a94ea17ff36b1675
Max <thewildstar.m@gmail.com>
2023-01-14 01:40:51 +0300

    second lesson completed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptiv;
+let title = "my project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 20;
+let rollback = 32;
+let fullPrice = 36000;
+let adaptiv = true;
+
+console.log(typeof title, typeof fullPrice, typeof adaptiv);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+console.log(screens.toLowerCase().split(" "));
+console.log(fullPrice * (rollback / 100));
