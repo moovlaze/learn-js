dde4869f58eecf506a9b3f2ec446480891a96cec
Max <thewildstar.m@gmail.com>
2023-01-15 22:51:08 +0300

    Динамическая типизация данных в Javascript

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
-let title = "my project";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 20;
+let title = prompt("как называется ваш проект?", "мой проект");
+let screens = prompt(
+	"Какие типы экранов нужно разработать?",
+	"Простые, Сложные, Интерактивные"
+);
+let screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
+let adaptiv = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt(
+	"Какой дополнительный тип услуги нужен?",
+	"натяжка на Wp"
+);
+let servicePrice1 = +prompt("Сколько это будет стоить?", "2500");
+let service2 = prompt(
+	"Какой дополнительный тип услуги нужен?",
+	"натяжка на Wp"
+);
+let servicePrice2 = +prompt("Сколько это будет стоить?", "2500");
+
 let rollback = 32;
-let fullPrice = 36000;
-let adaptiv = true;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
 
 console.log(typeof title, typeof fullPrice, typeof adaptiv);
 console.log(screens.length);
@@ -11,3 +26,19 @@ console.log(`Стоимость верстки экранов ${screenPrice} р�
 console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
 console.log(screens.toLowerCase().split(" "));
 console.log(fullPrice * (rollback / 100));
+console.log(servicePercentPrice);
+
+switch (true) {
+	case fullPrice >= 30000:
+		console.log("Даем скидку в 10%");
+		break;
+	case fullPrice >= 15000 && fullPrice < 30000:
+		console.log("Даем скидку в 5%");
+		break;
+	case fullPrice < 15000 && fullPrice >= 0:
+		console.log("Скидка не предусмотрена");
+		break;
+	default:
+		console.log("Что то пошло не так");
+		break;
+}
